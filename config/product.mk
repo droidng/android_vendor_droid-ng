7e07afdc39e7ff05b313c69f924fed2f001510e3
nift4 <nift4@protonmail.com>
2022-07-13 15:47:15 +0200

    Add ng iconpack
    This is hard work of our designer, Qirkl. Don't just kang this >:(

--- config/product.mk
+++ config/product.mk
@@ -1,2 +1,6 @@
 PRODUCT_BRAND ?= droid-ng
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/droid-ng/overlay/iconpack
+PRODUCT_PACKAGE_OVERLAYS += vendor/droid-ng/overlay/iconpack
+
 include vendor/droid-ng/sdk/common.mk
