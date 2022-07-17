13a280713cbe72f8519030ba249f2230924bb10c
nift4 <nift4@protonmail.com>
2022-07-17 18:10:54 +0200

    add make-ota-json.sh

---
+++ tools/make-ota-json.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+function get_val {
+	awk -F= -v value="$1" '!/#/ && $1 == value {OFS="=";$1="";print substr($0,2)}' "$2"
+}
+SYSTEM_BUILD_PROP="$OUT/system/build.prop"
+OTA_METADATA="$OUT/ota_metadata"
+function getprop {
+	get_val "$1" "$SYSTEM_BUILD_PROP"
+}
+function getmeta {
+	get_val "$1" "$OTA_METADATA"
+}
+
+ZIP="droid-ng-$(getprop ro.lineage.version).zip"
+ZIPPATH="$OUT/$ZIP"
+
+datetime="$(getmeta post-timestamp)"
+filename="$ZIP"
+id="$(cat "$ZIPPATH.sha256sum" | awk '{ print $1 }')"
+romtype="$(getprop ro.lineage.releasetype)"
+size="$(stat -c%s "$ZIPPATH")"
+url="https://bigota.droid-ng.eu.org/$id/$ZIP"
+version="$(getprop ro.lineage.build.version)"
+
+function print_field_str {
+	echo -n "    \"$1\": \"$(eval echo "\$$1")\""
+}
+function print_field_int {
+	echo -n "    \"$1\": $(eval echo "\$$1")"
+}
+echo {
+print_field_int datetime; echo ,
+print_field_str filename; echo ,
+print_field_str id; echo ,
+print_field_str romtype; echo ,
+print_field_str size; echo ,
+print_field_str url; echo ,
+print_field_str version; echo
+echo }
