42f6e9a6262ccc8049979e363bac12efc8997307
nift4 <nift4@protonmail.com>
2022-07-19 19:16:47 +0200

    add update-translations.sh

---
+++ tools/update-translations.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+unset CDPATH
+
+# https://stackoverflow.com/a/246128
+SOURCE=${BASH_SOURCE[0]}
+while [ -L "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+  SOURCE=$(readlink "$SOURCE")
+  [[ $SOURCE != /* ]] && SOURCE=$DIR/$SOURCE # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR=$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )
+
+cd "$DIR/.."
+git fetch ng ng-v3-l10n
+git merge --squash FETCH_HEAD
+git commit -m "Automated translation import"
