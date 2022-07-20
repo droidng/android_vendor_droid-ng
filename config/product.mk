ad837dd70b856658f0c0e88b393dbb2289c2c87e
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2022-07-20 15:57:27 +0200

    prebuilts: Introduce Jellyfish
    * due to heavy use of viewbinding and build flavours, its a prebuilt.
    * url: https://github.com/nift4/Jellyfish/tree/7b776c814727b1899583d9ba85bebbb62e6e2cc7
    * gradle target: assembleStyxBaseUndefRelease

--- config/product.mk
+++ config/product.mk
@@ -3,4 +3,7 @@ PRODUCT_BRAND ?= droid-ng
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/droid-ng/overlay/iconpack
 PRODUCT_PACKAGE_OVERLAYS += vendor/droid-ng/overlay/iconpack
 
+PRODUCT_PACKAGES += \
+    Jellyfish
+
 include vendor/droid-ng/sdk/common.mk
